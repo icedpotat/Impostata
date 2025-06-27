14e56843860a3f5726793e758103f283619bbb0f
tempor <soneme@yahoo.com>
2025-06-27 08:23:01 +0200

    Enable fullscreen mode and transparent status bar
    Set both MainActivity and GroupActivity to fullscreen by applying WindowManager flags in onCreate and specifying the app theme in the manifest. Updated the app theme to use transparent primary colors and status bar for a more immersive UI.

--- app/src/main/java/com/freeze/impostata/MainActivity.kt
+++ app/src/main/java/com/freeze/impostata/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.freeze.impostata
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
